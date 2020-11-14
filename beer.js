712669a375edca0a88e481ca5457a868807f9014
Cookicha <74389746+Cookicha@users.noreply.github.com>
2020-11-14 01:35:58 +0100

    Add files via upload

---
+++ beer.js
@@ -0,0 +1,186 @@
+$(document).ready(function() {
+ $('#li1').hover(function(){
+  $('#I').addClass('onfocus');
+  $('#one').addClass('onfocus');
+  $('#four').addClass('onfocus');
+  $('#seven').addClass('onfocus');
+  $('#fourteen').addClass('onfocus');
+  $('#twentyseven').addClass('onfocus');
+  $('#fortyone').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+
+ $('#li2').hover(function(){
+  $('#II').addClass('onfocus');
+  $('#two').addClass('onfocus');
+  $('#five').addClass('onfocus');
+  $('#eight').addClass('onfocus');
+  $('#fifteen').addClass('onfocus');
+  $('#twentyeight').addClass('onfocus');
+  $('#fortytwo').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+
+ $('#li3').hover(function(){
+  $('#III').addClass('onfocus');
+  $('#three').addClass('onfocus');
+  $('#six').addClass('onfocus');
+  $('#nine').addClass('onfocus');
+  $('#sixteen').addClass('onfocus');
+  $('#twentynine').addClass('onfocus');
+  $('#fortythree').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+
+ $('#li4').hover(function(){
+  $('#IV').addClass('onfocus');
+  $('#ten').addClass('onfocus');
+  $('#seventeen').addClass('onfocus');
+  $('#thirty').addClass('onfocus');
+  $('#fortyfour').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+
+ $('#li5').hover(function(){
+  $('#V').addClass('onfocus');
+  $('#eighteen').addClass('onfocus');
+  $('#thirtyone').addClass('onfocus');
+  $('#fortyfive').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+
+ $('#li6').hover(function(){
+  $('#VI').addClass('onfocus');
+  $('#nineteen').addClass('onfocus');
+  $('#thirtytwo').addClass('onfocus');
+  $('#fortysix').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+
+ $('#li7').hover(function(){
+  $('#VII').addClass('onfocus');
+  $('#twenty').addClass('onfocus');
+  $('#thirtythree').addClass('onfocus');
+  $('#fortyseven').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+
+ $('#li8').hover(function(){
+  $('#VIII').addClass('onfocus');
+  $('#thirtyfour').addClass('onfocus');
+  $('#fortyeight').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+
+ $('#li9').hover(function(){
+  $('#IX').addClass('onfocus');
+  $('#twentyone').addClass('onfocus');
+  $('#thirtyfive').addClass('onfocus');
+  $('#fortynine').addClass('onfocus');
+  $('#twentytwo').addClass('onfocus');
+  $('#thirtysix').addClass('onfocus');
+  $('#fifty').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+
+ $('#li10').hover(function(){
+  $('#X').addClass('onfocus');
+  $('#twentythree').addClass('onfocus');
+  $('#thirtyseven').addClass('onfocus');
+  $('#fiftyone').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+
+ $('#li11').hover(function(){
+  $('#XI').addClass('onfocus');
+  $('#eleven').addClass('onfocus');
+  $('#twentyfour').addClass('onfocus');
+  $('#thirtyeight').addClass('onfocus');
+  $('#fiftytwo').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+
+ $('#li12').hover(function(){
+  $('#XII').addClass('onfocus');
+  $('#twelve').addClass('onfocus');
+  $('#twentyfive').addClass('onfocus');
+  $('#thirtynine').addClass('onfocus');
+  $('#fiftythree').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+
+ $('#li13').hover(function(){
+  $('#XIII').addClass('onfocus');
+  $('#thirteen').addClass('onfocus');
+  $('#twentysix').addClass('onfocus');
+  $('#forty').addClass('onfocus');
+  $('#fiftyfour').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+
+ $('#li14').hover(function(){
+  $('#XIV').addClass('onfocus');
+  $('#fiftyfive').addClass('onfocus');
+  $('#sixtyone').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+
+ $('#li15').hover(function(){
+  $('#XV').addClass('onfocus');
+  $('#fiftysix').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+
+ $('#li16').hover(function(){
+  $('#XVI').addClass('onfocus');
+  $('#fiftyseven').addClass('onfocus');
+  $('#sixtytwo').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+
+ $('#li17').hover(function(){
+  $('#XVII').addClass('onfocus');
+  $('#fiftyeight').addClass('onfocus');
+  $('#sixtythree').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+
+ $('#li18').hover(function(){
+  $('#XVIII').addClass('onfocus');
+  $('#fiftynine').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+
+ $('#li19').hover(function(){
+  $('#XIX').addClass('onfocus');
+  $('#sixtyfour').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+
+ $('#li20').hover(function(){
+  $('#XX').addClass('onfocus');
+  $('#sixty').addClass('onfocus');
+  $('#sixtyfive').addClass('onfocus');
+ },function(){
+  $('*').removeClass('onfocus');
+ });
+});
