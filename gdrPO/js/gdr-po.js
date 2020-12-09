3527fbbfcfbfdd441b488b7eb969dafd6e79d835
Cookicha <74389746+Cookicha@users.noreply.github.com>
2020-12-09 16:44:08 +0100

    qsdfghjklm

--- gdrPO/js/gdr-po.js
+++ gdrPO/js/gdr-po.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $("#hamburger").click(function() {
-    $('.menuItem, #hamburgerSign, #crossSign').toggle(200);
+    $('.menuItem, #hamburgerSign, #crossSign').toggle();
+    $('.nav').toggleClass('open');
   });
 });
