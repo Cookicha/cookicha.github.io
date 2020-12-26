2604d395b55a2eb48e1945b92f1d8115b84d7e0d
Cookicha <74389746+Cookicha@users.noreply.github.com>
2020-12-26 21:59:53 +0100

    mlkjhg

--- gdrPO/js/gdr-po.js
+++ gdrPO/js/gdr-po.js
@@ -4,21 +4,3 @@ $(document).ready(function() {
     $('.nav').toggleClass('open');
   });
 });
-var requestURL = 'data/dataGdrPo.json';
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function() {
-  const data = request.response;
-  const labos = Object.values(data);
-  const ids = Object.keys(data);
-
-  var nombreDeLabos = ids.length;
-  var nombreDeChercheurs = 0;
-  for (var i = 0; i < ids.length; i++) {
-    var id = ids[i];
-    nombreDeChercheurs += id[membres].length;
-  }
-  console.log("labos : " + nombreDeLabos + ", chercheurs : " + nombreDeChercheurs);
-}
